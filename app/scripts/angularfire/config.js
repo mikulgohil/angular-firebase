95ba595fda95a31b7f39f6d7c1a5af4224c5a437
Mikul Gohil <mikulgohil@gmail.com>
2015-06-21 21:15:08 +0530

    Firebase init

---
+++ app/scripts/angularfire/config.js
@@ -0,0 +1,2 @@
+angular.module('firebase.config', [])
+  .constant('FBURL', 'https://ng-mikul-angular.firebaseio.com');
---
+++ app/scripts/app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name myAppApp
+ * @description
+ * # myAppApp
+ *
+ * Main module of the application.
+ */
+angular.module('myAppApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'firebase',
+    'firebase.ref'
+  ]);
